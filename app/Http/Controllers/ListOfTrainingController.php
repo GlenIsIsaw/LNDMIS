250233061d2464be07861d775ac3f23af8847620
YunChe05 <mikebarquilla8@gmail.com>
2022-09-07 11:11:53 +0800

    Non-Teaching in List Of Trainings

--- app/Http/Controllers/ListOfTrainingController.php
+++ app/Http/Controllers/ListOfTrainingController.php
@@ -16,6 +16,7 @@ class ListOfTrainingController extends Controller
 
         $listObject = DB::table('users')
                         ->join('list_of_trainings', 'users.id', '=', 'list_of_trainings.user_id')
+                        ->where('teacher', 'Yes')
                         ->orderBy('name','asc')
                         ->select('name', 'certificate_title', 'date_covered', 'level', 'num_hours')
                         ->get();
@@ -24,7 +25,11 @@ class ListOfTrainingController extends Controller
 
         $list = $grouped->toArray();
 
-        $templateProcessor = new TemplateProcessor('Certificate.docx');
+        $templateProcessor = new TemplateProcessor(storage_path('Certificate.docx'));
+
+        $templateProcessor->setValue('deptname','Institute of Computer Studies');
+        $templateProcessor->setValue('year','2020');
+        $templateProcessor->setValue('daterange','January - June 2020');
 
         $replacements = array();
         $i = 0;
@@ -41,20 +46,68 @@ class ListOfTrainingController extends Controller
         $templateProcessor->cloneBlock('table', count($replacements), true, false, $replacements);
 
         $i = 0;
-        foreach($list as $group) {
+        foreach($list as $group) 
+        {
             $values = array();
-            foreach($group as $row) {
+            foreach($group as $row) 
+            {
                 $values[] = array(
                     "certificate_title_{$i}" => $row->certificate_title,
                     "date_covered_{$i}" => $row->date_covered,
                     "level_{$i}" => $row->level,
-                    "num_hours_{$i}" => $row->num_hours
-        );
-    }
-    $templateProcessor->cloneRowAndSetValues("certificate_title_{$i}", $values);
+                    "num_hours_{$i}" => $row->num_hours);
+            }
+            $templateProcessor->cloneRowAndSetValues("certificate_title_{$i}", $values);
 
-    $i++;
+            $i++;
+        }
+
+
+
+
+        $listObject = DB::table('users')
+                        ->join('list_of_trainings', 'users.id', '=', 'list_of_trainings.user_id')
+                        ->where('teacher', 'No')
+                        ->orderBy('name','asc')
+                        ->select('name', 'certificate_title', 'date_covered', 'level', 'num_hours')
+                        ->get();
+
+        $grouped = $listObject->groupBy('name');
+
+        $list = $grouped->toArray();
+
+        $replacements = array();
+        $i = 0;
+        foreach($list as $name => $cert) {
+            $replacements[] = array(
+                'name2' => $name,
+                'certificate_title2' => '${certificate_title2_'.$i.'}',
+                'date_covered2' => '${date_covered2_'.$i.'}',
+                'level2' => '${level2_'.$i.'}',
+                'num_hours2' => '${num_hours2_'.$i.'}'
+    );
+                $i++;
 }
+        $templateProcessor->cloneBlock('table2', count($replacements), true, false, $replacements);
+
+        $i = 0;
+        foreach($list as $group) 
+        {
+            $values = array();
+            foreach($group as $row) 
+            {
+                $values[] = array(
+                    "certificate_title2_{$i}" => $row->certificate_title,
+                    "date_covered2_{$i}" => $row->date_covered,
+                    "level2_{$i}" => $row->level,
+                    "num_hours2_{$i}" => $row->num_hours);
+            }
+            $templateProcessor->cloneRowAndSetValues("certificate_title2_{$i}", $values);
+
+            $i++;
+        }
+
+    
         $templateProcessor->saveAs('ListOfTrainings.docx');
         return response()->download(public_path('ListOfTrainings.docx'))->deleteFileAfterSend(true);
 
