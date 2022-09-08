35a57fe2fb46d8148b48968c82cf0b8aaaeaac13
glen <glenpabico18@gmail.com>
2022-09-08 17:18:55 +0800

    Add Header and Footer

---
+++ resources/views/partials/_hero.blade.php
@@ -0,0 +1,18 @@
+
+<section
+class="relative h-40 bg-laravel flex flex-col justify-center align-center text-center space-y-4 mb-4"
+>
+<div
+    class="absolute top-0 left-0 w-full h-full opacity-10 bg-no-repeat bg-center"
+    style="background-image: url('images/cnsc.png')"
+></div>
+
+<div class="z-5">
+    <h1 class="center text-3xl font-bold uppercase text-white">
+        Camarines Norte State College
+
+        <br><span class="text-">Learning And Development</span>
+    </h1>
+    
+</div>
+</section>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,50 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="images/favicon.ico" />
-        <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-            integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-            crossorigin="anonymous"
-            referrerpolicy="no-referrer"
-        />
-        <script src="https://cdn.tailwindcss.com"></script>
-        <script>
-            tailwind.config = {
-                theme: {
-                    extend: {
-                        colors: {
-                            laravel: "#ef3b2d",
-                        },
-                    },
-                },
-            };
-        </script>
-        <title>Learning and Development Management Information System</title>
-    </head>
-    <body class="mb-48">
-        <nav class="flex justify-between items-center mb-4">
-            <a href="index.html"
-                ><img class="w-24" src="images/logo.png" alt="" class="logo"
-            /></a>
-            <ul class="flex space-x-6 mr-6 text-lg">
-                <li>
-                    <a href="register.html" class="hover:text-laravel"
-                        ><i class="fa-solid fa-user-plus"></i> Register</a
-                    >
-                </li>
-                <li>
-                    <a href="login.html" class="hover:text-laravel"
-                        ><i class="fa-solid fa-arrow-right-to-bracket"></i>
-                        Login</a
-                    >
-                </li>
-            </ul>
-        </nav>
+@extends('layout')
+
+
+@section('content')
+
+
+
 
         <main>
             <div class="mx-4">
@@ -55,7 +15,7 @@
                         <h2 class="text-2xl font-bold uppercase mb-1">
                             Register
                         </h2>
-                        <p class="mb-4">Create an account to post gigs</p>
+                        <p class="mb-4">Create an account to access</p>
                     </header>
 
                     <form method="POST" action="{{route('user.store')}}">
@@ -86,6 +46,8 @@
                                 <option value="No">No</option>
                               </select>
 
+                            </div>
+
                         @error('teacher')
                         <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                         @enderror
@@ -106,7 +68,7 @@
                         @enderror
                         <div class="mb-6">
                             <label for="college" class="inline-block text-lg mb-2">
-                                College
+                                College/Department
                             </label>
                             <input
                                 type="text"
@@ -223,3 +185,5 @@
         </footer>
     </body>
 </html>
+
+@endsection
