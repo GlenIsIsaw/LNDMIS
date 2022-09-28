c710ad1970e815cdcdc32d70ec78e465fcd12699
glen <glenpabico18@gmail.com>
2022-09-28 11:59:05 +0800

    Changes on some back-end

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,40 +4,40 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-import "./bootstrap";
-import { createApp } from "vue";
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import router from "./router/User";
-import User from "./body/User.vue";
-import LaravelVuePagination from "laravel-vue-pagination";
-
- /**
-  * Next, we will create a fresh Vue application instance. You may then begin
-  * registering components with the application instance so they are ready
-  * to use in your application's views. An example is included for you.
-  */
+ import "./bootstrap";
+ import { createApp } from "vue";
+ import axios from 'axios'
+ import VueAxios from 'vue-axios'
+ import router from "./router/User";
+ import User from "./body/User.vue";
+ import LaravelVuePagination from "laravel-vue-pagination";
  
-
-const app = createApp(User);
-app.use(VueAxios, axios);
-app.use(router);
-app.component('Pagination', LaravelVuePagination);
-app.mount("#user");
- /**
-  * The following block of code may be used to automatically register your
-  * Vue components. It will recursively scan this directory for the Vue
-  * components and automatically register them with their "basename".
-  *
-  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
-  */
+  /**
+   * Next, we will create a fresh Vue application instance. You may then begin
+   * registering components with the application instance so they are ready
+   * to use in your application's views. An example is included for you.
+   */
+  
  
- // Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
- //     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
- // });
- 
- /**
-  * Finally, we will attach the application instance to a HTML element with
-  * an "id" attribute of "app". This element is included with the "auth"
-  * scaffolding. Otherwise, you will need to add an element yourself.
-  */
+ const app = createApp(User);
+ app.use(VueAxios, axios);
+ app.use(router);
+ app.component('Pagination', LaravelVuePagination);
+ app.mount("#user");
+  /**
+   * The following block of code may be used to automatically register your
+   * Vue components. It will recursively scan this directory for the Vue
+   * components and automatically register them with their "basename".
+   *
+   * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+   */
+  
+  // Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
+  //     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
+  // });
+  
+  /**
+   * Finally, we will attach the application instance to a HTML element with
+   * an "id" attribute of "app". This element is included with the "auth"
+   * scaffolding. Otherwise, you will need to add an element yourself.
+   */
