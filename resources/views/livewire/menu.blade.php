6f0a96e9b8dbc0c86f166bbc7c2c0c7073c3181e
glen <glenpabico18@gmail.com>
2022-10-11 13:44:47 +0800

    Fixed Sidebar Designs

--- resources/views/livewire/menu.blade.php
+++ resources/views/livewire/menu.blade.php
@@ -4,7 +4,7 @@
 :root {
     --main-bg-color: #23b1b1;
     --main-text-color: hsl(0, 0%, 96%);
-    --second-text-color: #267a7a;
+    --second-text-color: #f6f6f6;
     --second-bg-color: #c1eeef;
   }
   
@@ -94,10 +94,12 @@
 
     background: #eee;
     border-radius: 8px;
+    
   }
 
   .sidebar li.active a, .sidebar li.active a.hover {
 
+    background:#f6f6f6;
     color:#0F2027;
 
   }
@@ -107,6 +109,22 @@
     color:#fff;
   }
   
+
+
+  #openPopup {
+  padding: 20px 30px;
+  background-color: #4cd2de00;
+  border-color: #fcfcfc00;
+  box-shadow: 1px 1px 1px rgba(255, 255, 255, 0);
+  white-space: normal;
+  font-size: 15px;
+  letter-spacing: 0.2px;
+}
+
+#openPopup:hover, #openPopup:active, #openPopup:focus {
+  background-color: #ffffff !important;
+  border-color: #ffffff00 !important;
+}
   
   
   
@@ -136,22 +154,21 @@
    
    <div class="shadow-sm navbar-expand-md" style="background-color:#06283D" id="sidebar-wrapper">
     <div class="sidebar-heading text-center">
-    <a href="/"
-                ><img class="d-inline-block align-text-top" src="images/cnsc.png" alt="" class="logo" width="97" height="94"
-            /></a>
     </div>
-    <div class="sidebar-heading text-center py-4 primary-text fs-7 fw-bold text-uppercase">learning and development</div>
+    
+    <div class="sidebar-heading text-center py-4 primary-text fs-7 fw-bold text-uppercase">MENU</div>
 
     <hr class="h-color mx-2">
-    <div class="sidebar my-3">
+    <div class="sidebar mx-auto">
 
+       
         <ul class="list-unstyled px-2">
 
-        <li class=""><a href="/" class="text-decoration-none px-3 py-2 fw-bold second-text  d-block"><i
-                class="fas fa-tachometer-alt me-2"></i>Dashboard</a></li>
+        <li class=""><button wire:click="trainIndex" id="openPopup" class="btn btn-link-light text-decoration-none px-3 py-2 fw-bold second-text d-block"><i
+                class="fas fa-tachometer-alt me-2"></i>Home</a></li>
 
                 
-                <li class=""><button wire:click="trainIndex" class="text-decoration-none px-3 py-2 second-text fw-bold d-block"><i
+                <li class=""><button wire:click="trainIndex" id="openPopup" class="btn btn-link-light text-decoration-none px-3 py-2 second-text fw-bold d-block"><i
                  class="fas fa-project-diagram me-2"></i>Training</button></li>
                  @if (auth()->user()->role_as == 1)
                  <select class="border border-info border-3 rounded" wire:model="string" wire:click="passData">
@@ -166,7 +183,7 @@
 
 
                 
-                 <li class=""><button wire:click="idpsIndex" class="text-decoration-none px-3 py-2 second-text fw-bold d-block"><i
+                 <li class=""><button wire:click="idpsIndex" id="openPopup" class="btn btn-link-light text-decoration-none px-3 py-2 second-text fw-bold d-block"><i
                 class="fas fa-chart-line me-2"></i>IDP</button></li>
                 
                 @if (auth()->user()->role_as == 1)
@@ -180,15 +197,15 @@
                   <i class="fas fa-arrow-right"></i>
                    Create Individual Development Plan </button></li>
 
-                <li class=""><a href="#" class="text-decoration-none px-3 py-2 second-text fw-bold d-block"><i class="fas fa-gift me-2"></i>Archives</a></li>
+                <li class=""><button wire:click="trainIndex" id="openPopup" class="btn btn-link-light text-decoration-none px-3 py-2 second-text fw-bold d-block"><i class="fas fa-gift me-2"></i>Archives</a></li>
 
                  </ul>
                
           <hr class="h-color mx-2">
           <ul class="list-unstyled px-2">
-          <li class=""><a href="#" class="text-decoration-none px-3 py-2 second-text fw-bold d-block"><i class="fas fa-paperclip me-2"></i>Core Competencies</a></li>
+          <li class=""><button wire:click="trainIndex" id="openPopup" class="btn btn-link-light text-decoration-none px-3 py-2 second-text fw-bold d-block"><i class="fas fa-trash me-2"></i>Recycles</a></li>
               
-          <li class=""><a href="#" class="text-decoration-none px-3 py-2 second-text fw-bold d-block"><i
+          <li class=""><button wire:click="trainIndex" id="openPopup" class="btn btn-link-light text-decoration-none px-3 py-2 second-text fw-bold d-block"><i
             class="fas fa-cog me-2"></i>Settings</a></li>
           </ul>
             
@@ -197,10 +214,10 @@
           <!-- Log Out -->
           <hr class="h-color mx-2">
           <ul class="list-unstyled px-2">
-          <li class=""><a href="{{ route('logout') }}"
+          <li class=""><button wire:click="{{ route('logout') }}"
             onclick="event.preventDefault();
                           document.getElementById('logout-form').submit();"
-             {{ __('Logout') }} class="text-decoration-none px-3 py-2 text-danger fw-bold d-block"><i class="fas fa-power-off me-2"></i>Log Out</a></li>
+             {{ __('Logout') }} id="openPopup" class="btn btn-link-light text-decoration-none px-3 py-2 text-danger fw-bold d-block"><i class="fas fa-power-off me-2"></i>Log Out</a></li>
                 
     </div>
 </div>
