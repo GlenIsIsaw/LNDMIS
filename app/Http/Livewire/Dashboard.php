0b6ac1896f7dd2d6c89d5bfe2530367f27edd526
Mike Ryan D. Barquilla <78713255+YunChe05@users.noreply.github.com>
2022-11-26 21:04:14 +0800

    Home Dashboard

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Livewire;
 
+use Carbon\Carbon;
+use App\Models\Idp;
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\ListOfTraining;
+use App\Models\IncomingTrainings;
 
 class Dashboard extends Component
 {
-    public $currentUrl, $toggle;
+    use WithPagination;
+    
+    protected $paginationTheme = 'bootstrap';
+    public $name, $year, $date, $file, $toggle, $currentUrl, $fileType, $start_date, $end_date, $invitation_id, $sponsor, $venue, $level, $level_others, $free, $amount, $date_covered;
+    public $filter_name, $filter_level, $filter_free;
+    public $competencies = [];
+    public $progress = [0=>"0%", 1=>"33.33%", 2=>"66.66%", 3=>"100%"];
+    //public $currentUrl, $toggle;
 
     protected $listeners = [
         'toggle' => 'open'
@@ -22,8 +34,118 @@ class Dashboard extends Component
         $this->currentUrl = url()->current();
         //dd($this->currentUrl);
     }
+
+    public function getComp(){
+        $array = [];
+        $temp = [];
+        $lists = Idp::select('idps.id as idp_id','user_id','name','competency','status','year')
+                ->join('users', 'users.id', '=', 'idps.user_id')
+                ->where('college_id',auth()->user()->college_id)
+                ->where('year', 'like', '%'.date('Y').'%')
+                ->where('user_id', auth()->user()->id)
+                //->WhereRaw("LOWER(name) LIKE '%".strtolower($this->name)."%'")
+                ->orderBy('name','asc')
+                ->get();
+        foreach ($lists as $value) {
+            array_push($temp, [$value->name => $value->competency]);
+        }
+        //$temp = $array->groupBy('name');
+        foreach ($temp as $value) {
+            foreach ($value as $key => $value) {
+                $array[$key] = $value;
+            }
+        }
+
+
+        return $array;
+    }
+    public function countTraining(){
+        $count = [];
+        $tick = [];
+        $array = $this->getComp();
+        $j=0;
+        foreach ($array as $key => $value) {
+            $i = 0;
+            foreach ($value as $item) {
+                $lists = ListOfTraining::join('users', 'users.id', '=', 'list_of_trainings.user_id')
+                ->join('attendance_forms', 'attendance_forms.list_of_training_id', '=', 'list_of_trainings.id')
+                ->join('idps', 'idps.id', '=', 'attendance_forms.idp_id')
+                ->where('college_id',auth()->user()->college_id)
+                ->where('list_of_trainings.status','Approved')
+                ->where('name', $key)
+                ->where('attendance_forms.competency', 'like', '%'.$item.'%')
+                ->orderBy('name','asc')
+                ->count();
+                $count[$j]['competency'][$item] = $lists;
+                if($lists){
+                    $i++;   
+                }
+                
+            }
+            $count[$j]['progress'] = $i;
+            $count[$j]['name'] = $key;
+            $j++;
+        }
+        //dd($count);
+        $this->competencies = $count;
+    }
+    public function fileType($filename){
+        $array = explode(".", $filename);
+        return strtolower(end($array));
+
+    }
+    public function show(int $id){
+        $lists = IncomingTrainings::where('college_id',auth()->user()->college_id)
+                ->where('id', $id)
+                ->first();
+
+        
+        $this->fileType = $this->fileType($lists->file);
+        $this->name = $lists->name;
+        $this->date = $lists->date;
+        $this->file = $lists->file;
+        $this->sponsor = $lists->sponsor;
+        $this->venue = $lists->venue;
+        $this->level = $lists->level;
+        $this->date_covered = $lists->date_covered;
+        $this->free = $lists->free;
+        $this->amount = $lists->amount;
+        
+        //dd($this->fileType);
+
+    }
+    public function resetFilter(){
+        $this->filter_name = null;
+        $this->start_date = null;
+        $this->end_date = null;
+        $this->filter_level = null;
+        $this->filter_free = null;
+
+    }
     public function render()
     {
-        return view('livewire.dashboard');
+        $this->countTraining();
+        //dd($this->getComp());
+        if ($this->start_date && $this->end_date) {
+            $start_date = Carbon::parse($this->start_date)->toDateTimeString();
+            $end_date = Carbon::parse($this->end_date)->toDateTimeString();
+            $lists = IncomingTrainings::where('college_id', auth()->user()->college_id)
+                ->WhereRaw("LOWER(name) LIKE '%".strtolower($this->filter_name)."%'")
+                ->where('level', 'like', '%'.$this->filter_level.'%')
+                ->where('free', 'like', '%'.$this->filter_free.'%')
+                ->where('date','>',date('Y-m-d'))
+                ->whereBetween('date',[$start_date,$end_date])
+                ->orderBy('updated_at')
+                ->paginate(5);
+        }else{
+            $lists = IncomingTrainings::where('college_id', auth()->user()->college_id)
+            ->WhereRaw("LOWER(name) LIKE '%".strtolower($this->filter_name)."%'")
+            ->where('level', 'like', '%'.$this->filter_level.'%')
+            ->where('free', 'like', '%'.$this->filter_free.'%')
+            ->where('date','>',date('Y-m-d'))
+            ->orderBy('updated_at')
+            ->paginate(5);
+        }
+        return view('livewire.dashboard', ['trainings' => $lists]);
     }
-}
+} 
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,59 +1,160 @@
-
 <div>
-    @include('livewire.menu')
-   
-    
-    <div class="row mx-5 mb-3 mt-2">
-        <div class="col-sm-6">
-          <div class="card h-100 border border-5 border-light shadow-lg">
-            <img src="/images/training.png" class="card-img-top" alt="Training">
-            <div class="card-body">
-              <h5 class="card-title">Training</h5>
-              <p class="card-text lh-base">Good Day! Start to upload your Training/s by clicking the button below. Training is a part of being the best version of yourself and you need to be the best version of yourself to be successful.  </p>
-              
-              <button class="btn-secondary text-uppercase fw-bold fs-6 border-3 rounded-pill border-white px-4 py-2 text-white dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="background-image: linear-gradient(to right, #c31432, #240b36); ">
-               Trainings
-              </button>
-    
-              <ul class="dropdown-menu rounded-3 border-light border-3" style="background-image: linear-gradient(to right, #ADA996, #F2F2F2, #DBDBDB, #EAEAEA);">
-                <li><a href="/training" role="button" class="dropdown-item fw-bolder text-dark">My Training/s</a></li>
-                
-              </ul>
-           
-            </div>
-            <div class="card-footer">
-              <small class="text-muted fst-italic">“Pain is temporary, Victory is eternal”</small>
-            </div>
+  @include('livewire.menu')
+  <div class="container py-3 px-5">
+      <div class="row">
+          <div class="col-md-12">
+              <div id="main-card">
+                <div class="card" >
+                  <div class="card-header">
+                    
+                      <div class="fw-bolder fs-3 float-start text-uppercase"> IDP Completion Rate of Year {{ date('Y') }}</div>
+
+                 
+                  </div>
+                  <div class="card-body text-center">
+                      <div class="table-responsive border-secondary border-3 text-center">
+                          <table class="table table-hover table-bordered border border-5 border-secondary table align-middle">
+                              <thead class="text-dark align-bottom shadow" style="background-color:#FEFCFF;">
+                                  <tr>
+                                      <th scope="col">Name</th>
+                                      <th scope="col">Progress</th>
+                                      <th scope="col">Competency</th>
+                                      <th scope="col">Number of Trainings</th>
+                                      
+                                  </tr>
+                              </thead>
+                              <tbody>
+                                  @forelse ($competencies as $idp)
+                                      <tr>
+                                          <td rowspan="4" class="fw-bold fs-5">{{$idp['name']}}</td>
+                                          <td rowspan="4"><span class="badge text-white fs-6 px-4 py-3" style="background-color: #800">{{$progress[$idp['progress']]}}</span></td>
+                                          @foreach ($idp['competency'] as $comp => $count)
+                                      </tr>
+                                      <tr>
+                                          <td class="text-uppercase fw-bold fs-5">{{$comp}}</td>
+                                          <td class="fw-bold fs-5">{{$count}}</td>
+                                          @endforeach
+                                          
+                                      </tr>
+                                  @empty    
+                                      <tr>
+                                          <td colspan="20">No Record/s Found</td>
+                                      </tr>
+                                  @endforelse
+                              </tbody>
+                          </table>
+                      </div>
+                  </div>
+                  <div class="card-footer">
+                  </div>
+              </div>
+                  <div class="card" >
+                      <div class="card-header">
+                              <div class="fw-bolder fs-3 float-start text-uppercase">Incoming Invitations for Trainings</div>
+                              <button type="button"style="background-color:#800;" class="btn-secondary float-end border-3 rounded-circle mt-2" wire:click="resetFilter"><i class='fas fa-redo'></i></button>
+                      </div>
+                      <div class="card-body">
+                              
+                        <button type="button" data-bs-toggle="modal" data-bs-target="#filterInvitationModal" class="btn-secondary float-end text-white rounded-3 shadow text-uppercase fs-6 fw-bold px-4 py-2 mx-2" style="background-color: #800;"><i class="fas fa-filter me-2"></i>Filter</button>
+
+
+                      </div>
+                      <div class="card-body text-center">
+                          <div class="table-responsive rounded-3 text-center">
+                              @php
+                                  $array = [0 => 'Yes', 1 => 'No'];
+                              @endphp
+                              <table class="table border border-secondary border-5 table-hover">
+                                  <thead class="text-dark shadow" style="background-color:#FEFCFF;">
+                                      <tr>
+                                          <th scope="col">Seminar Title</th>
+                                          <th scope="col">Level</th>
+                                          <th scope="col">Sponsors</th>
+                                          <th scope="col">Venue</th>
+                                          <th scope="col">Free</th>
+                                          <th scope="col">Amount</th>
+                                          <th scope="col">Date Covered</th>
+                                          <th scope="col">Attachments</th>
+                                          
+                                      </tr>
+                                  </thead>
+                                  <tbody class="table align-middle">
+
+                                      @forelse ($trainings as $training)
+                                          <tr>
+                                              <td class="fw-bold">{{$training->name}}</td>
+                                              <td>{{$training->level}}</td>
+                                              <td>{{$training->sponsor}}</td>
+                                              <td>{{$training->venue}}</td>
+                                              <td>{{$array[$training->free]}}</td>
+                                              <td>{{$training->amount}}</td>
+                                              <td>{{$training->date_covered}}</td>
+                                              <td>
+                                                  <button type="button" class="btn btn-link" data-bs-toggle="modal" data-bs-target="#showInvitationModal" wire:click="show({{$training->id}})" style="color: #800">
+                                                      View File Attachment Here
+                                                 </button>
+                                              </td>
+                                          </tr>
+
+                                      @empty    
+                                          <tr>
+                                              <td colspan="20">No Record/s Found</td>
+                                          </tr>
+                                      @endforelse
+                                  </tbody>
+                              </table>
+                          </div>
+                      </div>
+                      <div class="card-footer">
+                          <p class="fst-italic fw-lighter text-capitalize text-muted text-sm-left"> Disregard If You already read the attachments </p>
+                          <div class="d-flex justify-content-center">
+                              {{ $trainings->links() }}
+                          </div>
+                      </div>
+                  </div>
+              </div>
           </div>
-        </div>
-    
-    
-        <div class="col-sm-6">
-          <div class="card h-100 border border-5 border-light shadow-lg">
-            <img src="/images/idp.png" class="card-img-top" alt="IDP">
-            <div class="card-body">
-              <h5 class="card-title">Individual Development Plan</h5>
-              <p class="card-text">Good Day! Start to upload your Training/s by clicking the button below. Plan your work and work your plan. “If you don't know where you are going, you'll end up someplace else.” </p>
-              <button class="btn-secondary text-uppercase fw-bold fs-6 border-3 rounded-pill border-white px-4 py-2 text-white dropdown-toggle mt-3" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="background-image: linear-gradient(to right, #c31432, #240b36); ">
-                IDP
-               </button>
-     
-               <ul class="dropdown-menu rounded-3 border-light border-3" style="background-image: linear-gradient(to right, #ADA996, #F2F2F2, #DBDBDB, #EAEAEA);">
-                <li><a href="/idp" role="button" class="dropdown-item fw-bolder text-dark" wire:click="myIDP">My IDP</a></li>
-                
-              </ul>
-    
-    
-            </div>
-            <div class="card-footer">
-              <small class="text-muted fst-italic">"Justice, will be served."</small>
-            </div>
-          </div>
-        </div>
-        
       </div>
-    @include('livewire.main-modal')
+  </div>
+
+
+  @include('livewire.invitation-modal')
+  @include('livewire.main-modal')
+  @section('script')
+  <script>
+      window.addEventListener('toggle', event => {
+          if(document.getElementById("wrapper").classList.contains('toggled')){
+              document.getElementById('main-card').className = 'vw-100';
+              document.getElementById('main-card').style.paddingRight = '15%';
+          }else{
+              document.getElementById('main-card').className = 'w-100';
+              document.getElementById('main-card').style.paddingRight = '0%';
+          }
+      })
+      window.addEventListener('close-modal', event => {
+
+          $('#createConfirmationInvitationModal').modal('hide');
+          $('#editConfirmationInvitationModal').modal('hide');
+          $('#deleteConfirmationInvitationModal').modal('hide');
+          $('#createConfirmationInvitationModal').modal('hide');
+          
+          
+          $('#notificationModal').modal('hide');
+          
+          
+      })
+      window.addEventListener('show-notification', event => {
+          $('#notificationModal').modal('show');
+      })
+      window.addEventListener('confirmation-create-training', event => {
+          $('#createConfirmationTrainingModal').modal('show');
+      })
+  </script>
+
+  @endsection
 </div>
+
+
 
 
         
